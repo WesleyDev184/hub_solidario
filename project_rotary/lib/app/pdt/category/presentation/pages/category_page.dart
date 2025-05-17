ca0214eb5e046c3190619e5fd4582a7ae453e5ed
Wesley Antonio <WesleyDev184>
2025-05-17 13:04:50 -0300

    up

--- project_rotary/lib/app/pdt/category/presentation/pages/category_page.dart
+++ project_rotary/lib/app/pdt/category/presentation/pages/category_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:project_rotary/core/components/appbar_custom.dart';
+import 'package:project_rotary/core/components/button.dart';
+import 'package:project_rotary/core/theme/custom_colors.dart';
 
 class CategoryPage extends StatelessWidget {
   final String categoryId;
@@ -16,10 +19,38 @@ class CategoryPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBarCustom(title: categoryTitle),
       backgroundColor: Colors.transparent,
-      body: Center(
-        child: Text(
-          'Detalhes da categoria: $categoryTitle (ID: $categoryId)',
-          style: const TextStyle(fontSize: 16),
+      body: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(
+                  color: CustomColors.primary,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              Button(
+                onPressed: () {},
+                text: "Entrar",
+                icon: Icon(LucideIcons.logIn, color: CustomColors.white),
+                backgroundColor: CustomColors.success,
+                isFullWidth: true,
+              ),
+            ],
+          ),
         ),
       ),
     );
