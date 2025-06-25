c9ae5c6bfe622878d6b6a5e948b1996c433eed10
WesleyDev184 <wa.wesley01@gmail.com>
2025-06-25 01:52:47 -0300

    att

--- api/Extensions/BuilderExtensions.cs
+++ api/Extensions/BuilderExtensions.cs
@@ -28,7 +28,7 @@ namespace api.Extensions
             new OpenApiInfo
             {
               Title = "Rotary APi",
-              Version = "v1.2",
+              Version = "v1.3",
               Description = "API for Rotary Club orthosis loan management system",
             });
           c.EnableAnnotations();
