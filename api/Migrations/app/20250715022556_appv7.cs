5278a6f6506bdf93d89e568ee2948be6f14cf7c4
WesleyDev184 <wa.wesley01@gmail.com>
2025-07-14 23:26:02 -0300

    att

---
+++ api/Migrations/app/20250715022556_appv7.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations.app
+{
+    /// <inheritdoc />
+    public partial class appv7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Applicants_ApplicantId",
+                table: "Loans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Items_ItemId",
+                table: "Loans");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Applicants_ApplicantId",
+                table: "Loans",
+                column: "ApplicantId",
+                principalTable: "Applicants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Items_ItemId",
+                table: "Loans",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Applicants_ApplicantId",
+                table: "Loans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Items_ItemId",
+                table: "Loans");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Applicants_ApplicantId",
+                table: "Loans",
+                column: "ApplicantId",
+                principalTable: "Applicants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Items_ItemId",
+                table: "Loans",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
