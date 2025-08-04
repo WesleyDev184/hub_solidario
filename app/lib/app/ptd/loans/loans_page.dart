36677c05e23f8683eaa140173ed670492da2faa1
Wesley Antonio <wa.wesley01@gmail.com>
2025-08-04 08:14:48 -0300

    att

--- app/lib/app/ptd/loans/loans_page.dart
+++ app/lib/app/ptd/loans/loans_page.dart
@@ -3,11 +3,9 @@ import 'package:app/core/api/loans/controllers/loans_controller.dart';
 import 'package:app/core/widgets/appbar_custom.dart';
 import 'package:app/core/widgets/input_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 
-// ...existing code...
 class LoansPage extends StatefulWidget {
   const LoansPage({super.key});
 
@@ -77,33 +75,22 @@ class _LoansPageState extends State<LoansPage> {
                     ),
                   );
                 }
-                return AnimationLimiter(
-                  child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: filteredLoans.length,
-                    itemBuilder: (context, index) {
-                      final loan = filteredLoans[index];
-                      return AnimationConfiguration.staggeredList(
-                        position: index,
-                        duration: const Duration(milliseconds: 500),
-                        child: SlideAnimation(
-                          verticalOffset: 50.0,
-                          child: FadeInAnimation(
-                            child: Padding(
-                              padding: const EdgeInsets.only(bottom: 5.0),
-                              child: LoanCard(
-                                id: loan.id.toString(),
-                                imageUrl: "assets/images/cr.jpg", // URL padrão
-                                loan: loan,
-                                loadData: () => loansController.loadLoans(),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: filteredLoans.length,
+                  itemBuilder: (context, index) {
+                    final loan = filteredLoans[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: LoanCard(
+                        id: loan.id.toString(),
+                        imageUrl: "assets/images/cr.jpg", // URL padrão
+                        loan: loan,
+                        loadData: () => loansController.loadLoans(),
+                      ),
+                    );
+                  },
                 );
               }),
             ),
