e4af162a69e1f97384f35a68e39774838b7eedfb
Wesley Antonio <wa.wesley01@gmail.com>
2025-08-13 23:26:38 -0300

    att

--- jobs/loan-expiration-validator/main.go
+++ jobs/loan-expiration-validator/main.go
@@ -4,45 +4,60 @@ import (
 	"context"
 	"log"
 
-	"firebase.google.com/go/v4"
+	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/messaging"
 	"google.golang.org/api/option"
 )
 
-func main() {
-	// Inicialize o SDK Admin do Firebase
-	opt := option.WithCredentialsFile("./hubapp.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		log.Fatalf("erro ao inicializar o app: %v\n", err)
-	}
+type AppNotification struct {
+	Title string
+	Body  string
+	Token string
+	Data  map[string]string
+}
 
-	// Obtenha um cliente para o Firebase Cloud Messaging
-	ctx := context.Background()
-	client, err := app.Messaging(ctx)
-	if err != nil {
-		log.Fatalf("erro ao obter o cliente de Messaging: %v\n", err)
-	}
-
-	// O token de registro do dispositivo para o qual você deseja enviar a notificação.
-	// Este token é gerado pelo SDK do Firebase no seu aplicativo cliente.
-	// Você precisará obtê-lo do seu aplicativo cliente e enviá-lo para o seu servidor.
-	registrationToken := "dBzbyoOZRrye-_jn3APqSd:APA91bEv_nSGAv6yBA-MgyoZCi52DqbYh5t4K-pb13t3r2iggOroup1pKqoR3js5kWBp55zmmo8cl95CP1y02GeZqG0l_a02MWsy7UCZBz8-jXCUR6et7ec"
-
-	// Crie a mensagem a ser enviada
+func SendNotification(ctx context.Context, client *messaging.Client, notif AppNotification) (string, error) {
 	message := &messaging.Message{
 		Notification: &messaging.Notification{
-			Title: "Olá do Go!",
-			Body:  "Esta é uma notificação enviada do nosso servidor Go!",
+			Title: notif.Title,
+			Body:  notif.Body,
 		},
-		Token: registrationToken,
+		Token: notif.Token,
+		Data:  notif.Data,
+	}
+	return client.Send(ctx, message)
+}
+
+func configureFirebase(ctx context.Context, credFile string) (*messaging.Client, error) {
+	opt := option.WithCredentialsFile(credFile)
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		return nil, err
+	}
+	client, err := app.Messaging(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func main() {
+	ctx := context.Background()
+	client, err := configureFirebase(ctx, "./hubapp.json")
+	if err != nil {
+		log.Fatalf("erro ao configurar o Firebase: %v\n", err)
 	}
 
-	// Envie a mensagem
-	response, err := client.Send(ctx, message)
+	notif := AppNotification{
+		Title: "Olá do Go!",
+		Body:  "Esta é uma notificação enviada do nosso servidor Go!",
+		Token: "dBzbyoOZRrye-_jn3APqSd:APA91bEv_nSGAv6yBA-MgyoZCi52DqbYh5t4K-pb13t3r2iggOroup1pKqoR3js5kWBp55zmmo8cl95CP1y02GeZqG0l_a02MWsy7UCZBz8-jXCUR6et7ec",
+		Data:  map[string]string{"route": "/ptd/loans"},
+	}
+
+	response, err := SendNotification(ctx, client, notif)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// A resposta contém o ID da mensagem
 	log.Println("Notificação enviada com sucesso:", response)
 }
